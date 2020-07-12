6d7dee507b8f7377f199ff8ba9892cec9bde3aef
Phillip Strom <phillip.strom@gmail.com>
2020-07-12 16:01:43 -0600

    Finish user story 17 functionality

--- spec/features/apps/new_spec.rb
+++ spec/features/apps/new_spec.rb
@@ -16,11 +16,9 @@ RSpec.describe "new application page" do
     click_button "Add to Favorites"
     expect(page).to have_content("Favorites: 2")
 
-    visit "/apps/new" # for test without favorite index only
-    # visit "/favorites"
-    # click_on "Apply Now"
+    visit "/favorites"
+    click_on "Apply Now"
     expect(current_path).to eq("/apps/new")
-
 
     select(@dog.name)
     select(@dog2.name)
@@ -37,11 +35,31 @@ RSpec.describe "new application page" do
     expect(current_path).to eq("/favorites")
     expect(page).to have_content("Application submitted successfully")
 
-    # within class
     expect(page).to_not have_content(@dog.name)
     expect(page).to_not have_content(@dog2.name)
   end
   it "should flash error and return to new app when incomplete form" do
+    visit "/pets/#{@dog.id}"
+    click_button "Add to Favorites"
+    visit "/pets/#{@dog2.id}"
+    click_button "Add to Favorites"
+    expect(page).to have_content("Favorites: 2")
 
+    visit "/favorites"
+    click_on "Apply Now"
+    expect(current_path).to eq("/apps/new")
+
+    select(@dog.name)
+    fill_in :name, with: "Name"
+    fill_in :address, with: "Address"
+    fill_in :city, with: "City"
+    fill_in :state, with: "State"
+    fill_in :zip, with: "Zip"
+    fill_in :phone_number, with: "Phone Number"
+
+    click_button "Submit"
+
+    expect(page).to have_content("Application not submitted")
+    expect(current_path).to eq("/apps/new")
   end
 end
