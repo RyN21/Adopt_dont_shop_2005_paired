f574e3988768174da4c8984e04ee2f2d6801d0c3
Phillip Strom <phillip.strom@gmail.com>
2020-07-13 18:33:06 -0600

    Create User Story 21 functionality

--- spec/features/pet_apps_spec.rb
+++ spec/features/pet_apps_spec.rb
@@ -23,11 +23,21 @@ RSpec.describe "pet applications index page" do
     expect(page).to have_link(@app.name, href: "/apps/#{@app.id}")
     expect(page).to have_link(@app2.name, href: "/apps/#{@app2.id}")
   end
+  it "displays a message for no applications" do
+    @shelter_1 = Shelter.create!(name: "Paw Pals", address: "123 Main Street", city: "Denver", state: "CO", zip_code: 80202)
+    @dog = Pet.create!(name: "Jake", age: 4, sex: "male", image: "https://epi.azureedge.net/website-images/images/default-album/standard-poodle.jpg?sfvrsn=abed37b_2", shelter_id: @shelter_1.id)
+    @dog2 = Pet.create!(name: "Charley", age: 5, sex: "male", image: "https://www.pennmedicine.org/news/-/media/images/pr%20news/news/2018/may/summerdog.ashx?h=263&w=400&la=en", shelter_id: @shelter_1.id)
+    @app = App.create!(name: "John",
+      address: "123 Main St", city: "Denver", state: "CO", zip: "80202", phone_number: "123-456-7890",
+      description: "I like dogs", pet_ids: ["#{@dog.id}"])
+
+    visit "/pets/#{@dog2.id}"
+    click_on "Applications"
+
+    expect(current_path).to eq("/pets/#{@dog2.id}/apps")
+
+    expect(page).to_not have_content(@app.name)
+    expect(page).to have_content("No Applications for #{@dog2.name}")
+
+  end
 end
-
-
-# User Story 21, Pet Applications Index Page When No Applications
-#
-# As a visitor
-# When I visit a pet applications index page for a pet that has no applications on them
-# I see a message saying that there are no applications for this pet yet
